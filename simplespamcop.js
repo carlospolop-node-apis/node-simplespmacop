d21fedce46c6357ba5c0cce3db7d2f1964f9f4b7
carlospolop <carlospolop@gmail.com>
2018-04-21 14:13:12 +0200

    SpamcopV1

--- simplespamcop.js
+++ simplespamcop.js
@@ -7,7 +7,7 @@ function checkIP(ip){
             if (error) reject(Error("Error using spamcop: "+error));
             if (body){
                 if (body.indexOf("not listed") != -1) resolve({malicious: true, title: "Spammer"});
-                resolve({malicious: false, title: "Spammer"});
+                resolve({malicious: false, title: "Not Malicious"});
             }
         });
     });
